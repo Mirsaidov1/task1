f0ecb1a513e389f81dd4a3e6ded7dd5620873dd3
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2024-12-29 23:30:59 +0500

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,101 @@
+﻿
+
+//int age = 20;
+//age = 22;
+//Console.WriteLine($"Привет, мне {age} лет.");
+
+//Console.WriteLine("Введите число: ");//Begin1
+//int a = int.Parse(Console.ReadLine());
+//int P = 4 * a;
+//Console.WriteLine($"Периметр квадрата: {P}");
+
+
+
+Console.WriteLine("Введите число: ");   //Begin2
+int b = int.Parse(Console.ReadLine());
+double S = Math.Pow(b, 2);
+Console.WriteLine($"Площадь квадрата: {S}");
+
+
+
+//Console.WriteLine("Введите число a: "); //Begin3
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите число b: ");
+//int b = int.Parse(Console.ReadLine());
+//int S = a * b;
+//int P = 2 * (a + b);
+//Console.WriteLine($"Площадь: {S}");
+//Console.WriteLine($"Периметр: {P}");
+
+
+
+//double pi = 3.14;                  //Begin4
+//Console.WriteLine("Введите диаметр окружности: ");
+//int d = int.Parse(Console.ReadLine());
+//double L = pi * d;
+//Console.WriteLine($"Длина: {L}");
+
+
+
+//Console.WriteLine("Введите число: "); //Begin5
+//int a = int.Parse(Console.ReadLine());
+//double V = Math.Pow(a, 3);
+//double S = 6 * Math.Pow(a, 2);
+//Console.WriteLine($"{V}");
+//Console.WriteLine($"{S}");
+
+
+
+//Console.WriteLine("Введите число a: ");//Begin6
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите число b: ");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите число c: ");
+//int c = int.Parse(Console.ReadLine());
+//int V = a * b * c;
+//int S = 2 * ((a * b) + (b * c) + (a * c));
+//Console.WriteLine($"Объем = {V}");
+//Console.WriteLine($"Площадь = {S}");
+
+
+//double pi = 3.14; //Begin7
+//Console.WriteLine("Введите радиус R: "); 
+//int r = int.Parse(Console.ReadLine());
+//double L = 2 * pi * r;
+//double S = pi * Math.Pow(r, 2);
+//Console.WriteLine($"Длина круга: {L}");
+//Console.WriteLine($"Площадь круга: {S}");
+
+
+
+//Console.WriteLine("Введите число a: "); //Begin8
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите число b: ");
+//int b = int.Parse(Console.ReadLine());
+//int C = (a + b) / 2;
+//Console.WriteLine($"Среднее арифметическое число: {C}");
+
+
+
+//Console.WriteLine("Введите неотрицательное число a: "); //Begin9
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите неотрицательное число b: ");
+//int b = int.Parse(Console.ReadLine());
+//double c = Math.Sqrt(a * b);
+//Console.WriteLine($"Квадратный корень из их произведения: {c}");
+
+
+
+//Console.WriteLine("Введите массу в кг: "); //integer2
+//int m = int.Parse(Console.ReadLine());
+//int summa = m / 1000;
+//Console.WriteLine($"Масса в тоннах: {summa}");
+
+
+
+//Console.WriteLine("Введите число А: ");//integer4
+//int A = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите число B: ");
+//int B = int.Parse(Console.ReadLine());
+//int C = A / B;
+//Console.WriteLine($"Количество отрезков B, размещенных на А: {C}");
