9116348e6e265ad5a2642f8378e5e3fc3e9e5702
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-08 12:11:08 +0500

    ДЗ_День_5

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,26 +1,39 @@
-﻿//2.Количество четных чисел
+﻿// 2. Количество четных и не четных чисел в одномерном массиве
 int[] array = { 121, 234, 1221, 456, 989 };
 int a = 0;
+int a1 = 0;
 foreach (var item in array)
 {
     if (item % 2 == 0)
     {
-        Console.WriteLine(item);
         a++;
     }
-}
-Console.WriteLine("Количество четных чисел:  " + a);
-// 2. Количество не четных чисел
-int n = 0;
-foreach (var item in array)
-{
-    if (item % 2 > 0)
+    else if(item % 2 > 0)
     {
-        Console.WriteLine(item);
-        n++;
+        a1++;
     }
+    Console.WriteLine(item);
 }
-Console.WriteLine("Количество не четных чисел:  " + n);
+Console.WriteLine($"Количество четных чисел:{a}");
+Console.WriteLine($"Количество не четных чисел:{a1}\n");
+// Количество четных и не четных чисел в многомерном массиве
+int[,] arr1 = { { 1, 2, 3 }, { 4, 3, 5 }, { 5, 4, 6 } };
+int count = 0;
+int countt = 0;
+foreach (var i in arr1)
+{
+    if (i % 2 == 0)
+    {
+        count++;
+    }
+    else if (i % 2 > 0)
+    {
+        countt++;
+    }
+    Console.WriteLine(i);
+}
+Console.WriteLine($"Количество четных чисел: {count}");
+Console.WriteLine($"Количество не четных чисел: {countt}\n");
 // 3. MIN и MAX в многомерном массиве
 int[,] array1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 int max = array1[0, 0];
@@ -36,8 +49,8 @@ foreach (int item in array1)
         min = item;
     }
 }
-Console.WriteLine(max);
-Console.WriteLine(min);
+Console.WriteLine($"{max}");
+Console.WriteLine($"{min}\n");
 
 
 
