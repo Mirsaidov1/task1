d10f37db13464f098c6ab8ce0e7bae4127adb364
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-14 09:54:20 +0500

    ДЗ_День_7

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,58 +1,83 @@
-﻿// ДЗ_День_7
+﻿using System.Globalization;
 using System.Text;
 
-Console.WriteLine("Введите слово: ");
-string input = Convert.ToString(Console.ReadLine());
+//Console.WriteLine("Введите слово: ");
+//string input = Console.ReadLine() ?? "";
 
-StringBuilder result = new StringBuilder();
-string vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
-foreach (char c in input)
-{
-    if (vowels.Contains(c))
-    {
-        result.Append('*');
-    }
-    else if (c == ' ')
-    {
-        result.Append('-');
-    }
-    else
-        result.Append(c);
-}
-int wordcount = input.Length;
+//StringBuilder result = new StringBuilder();
+//string vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
 
-string longesword = "";
-foreach (string word in input)
-{
-    if (word.Length > longesword.Length)
-    {
-        longesword = word;
-    }
-}
 
-int digcount = 0;
-foreach (char n in input)
-{
-    if (char.IsDigit(n))
-    {
-        digcount++;
-    }
-}
+//foreach (char c in input)
+//{
+//    if (vowels.Contains(c))
+//    {
+//        result.Append('*');
+//    }
+//    else if (c == ' ')
+//    {
+//        result.Append('-');
+//    }
+//    else
+//    {
+//        result.Append(c);
+//    }
+//}
 
-Console.WriteLine(result + "\n");
-Console.WriteLine("Количество слов: " + wordcount);
-Console.WriteLine("Самое длинное слово: " + longesword);
-Console.WriteLine("Количество цифр: " + digcount);
-// string result = input.Replace('а','*')
-//                      .Replace('о','*')
-//                      .Replace('у','*')
-//                      .Replace('е','*')
-//                      .Replace('ё','*')
-//                      .Replace('э','*')
-//                      .Replace('и','*')
-//                      .Replace('ю','*')
-//                      .Replace('я','*')
-//                      .Replace('ы','*')
-//                      .Replace(' ','-');
-// System.Console.WriteLine(result);
+//int wordcount = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+//string longesword = "";
+//foreach (string word in input.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+//{
+//    if (word.Length > longesword.Length)
+//    {
+//        longesword = word;
+//    }
+//}
+
+//int digcount = 0;
+//foreach (char n in input)
+//{
+//    if (char.IsDigit(n))
+//    {
+//        digcount++;
+//    }
+//}
+//Console.WriteLine(result.ToString() + "\n");
+//Console.WriteLine("Количество слов: " + wordcount);
+//Console.WriteLine("Самое длинное слово: " + longesword);
+//Console.WriteLine("Количество цифр: " + digcount);
+//string result = input.Replace('а', '*')
+//                     .Replace('о', '*')
+//                     .Replace('у', '*')
+//                     .Replace('е', '*')
+//                     .Replace('ё', '*')
+//                     .Replace('э', '*')
+//                     .Replace('и', '*')
+//                     .Replace('ю', '*')
+//                     .Replace('я', '*')
+//                     .Replace('ы', '*')
+//                     .Replace(' ', '-');
+//Console.WriteLine(result.ToString());
+
+
+CultureInfo russianCulture = new CultureInfo("ru-RU");
+DateTime dateTime = DateTime.Now;
+string dayOfWeek = dateTime.ToString("dddd", russianCulture);
+dayOfWeek = char.ToUpper(dayOfWeek[0]) + dayOfWeek.Substring(1);
+Console.WriteLine($"Сегодня: {dateTime.ToString("dd.MM.yyyy 'года'")}");
+Console.WriteLine($"День недели: {dayOfWeek}");
+
+
+
+
+
+
+//int length = 1000000;
+//StringBuilder sb = new StringBuilder(length);
+//for (int i = 0; i < length; i++)
+//{
+//    sb.Append('A');
+//}
+//string result = sb.ToString();
+//Console.WriteLine("Длина созданной строки: " + result.Length);
 
