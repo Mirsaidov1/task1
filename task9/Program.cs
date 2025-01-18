e3edfa37f033fa50dc6869f4e812eab735167049
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-18 12:33:51 +0500

    ДЗ_День_9

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,10 +1,22 @@
-﻿Console.WriteLine("Введите температуру в Цельсиях: ");
+﻿// Задание_1
+Console.WriteLine("Введите температуру в Цельсиях: ");
 double celsiusInput = Convert.ToDouble(Console.ReadLine());
-double fahrenheitOutput = TemperatureConverter.CelsiusToFahrenheit(celsiusInput); 
+double fahrenheitOutput = TemperatureConverter.CelsiusToFahrenheit(celsiusInput);
 Console.WriteLine($"\n{celsiusInput}°C = {fahrenheitOutput}°F");
 
 Console.WriteLine("Введите температуру в Фаренгейтах: ");
 double fahrenheitInput = Convert.ToDouble(Console.ReadLine());
-double celsiusOutput = TemperatureConverter.FahrenheitToCelsius(fahrenheitInput); 
+double celsiusOutput = TemperatureConverter.FahrenheitToCelsius(fahrenheitInput);
 Console.WriteLine($"\n{fahrenheitInput}°F = {celsiusOutput}°C");
+// Задание_2
+Car car = new Car
+{
+    Mark = "Mers",
+    Model = "w124",
+    Year = 1993
+};
 
+//void (mark, model, year) = car;
+car.Deconstruct(out string mark, out string model, out int year);
+Console.WriteLine($"Марка: {mark}, Модель: {model}, Год выпуска {year}.");
+ 
---
+++ task9/models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Car
+{
+	public string Mark { get; set; }
+	public string Model { get; set; }
+	public int Year { get; set; }
+
+	public void Deconstruct(out string mark, out string model, out int year)
+	{
+		mark = Mark;
+		model = Model;
+		year = Year;
+	}
+
+
+	public Car()
+	{
+	}
+}
