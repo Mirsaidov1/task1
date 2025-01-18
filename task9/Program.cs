db93cdaa933fad1ab278ba1d8b5308bd198aa241
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-18 12:14:07 +0500

    ДЗ_День_9

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите температуру в Цельсиях: ");
+double celsiusInput = Convert.ToDouble(Console.ReadLine());
+double fahrenheitOutput = TemperatureConverter.CelsiusToFahrenheit(celsiusInput); 
+Console.WriteLine($"\n{celsiusInput}°C = {fahrenheitOutput}°F");
+
+Console.WriteLine("Введите температуру в Фаренгейтах: ");
+double fahrenheitInput = Convert.ToDouble(Console.ReadLine());
+double celsiusOutput = TemperatureConverter.FahrenheitToCelsius(fahrenheitInput); 
+Console.WriteLine($"\n{fahrenheitInput}°F = {celsiusOutput}°C");
+
---
+++ task9/models/TemperatureConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+static class TemperatureConverter
+{
+	public static double CelsiusToFahrenheit(double celsius)
+	{
+		return celsius * 9 / 5 + 32;
+	}
+
+	public static double FahrenheitToCelsius(double fahrenheit)
+	{
+		return (fahrenheit - 32) * 5 / 9; 
+	}
+
+    static TemperatureConverter()
+	{
+	}
+}
