b0d7e9b676efdae7eeaac34c0e7b12a5b175b674
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-21 00:06:19 +0500

    День_11

---
+++ day_11/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int sum = 0;
+foreach (var number in numbers)
+{
+    sum += number;
+}
+Console.WriteLine(sum);
+
+
+Dictionary<string, int> dict = new Dictionary<string, int>
+{
+    {"Mirsaidov", 100 },
+    {"Radzhabov", 98 },
+    {"Bozorov", 90 }
+};
+Console.WriteLine(string.Join(", \n", dict.Select(x => $"Студент: {x.Key} Балл: {x.Value}")));
+
