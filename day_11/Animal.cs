4fe59d2e356ced84858f6dcb616e8b869cb20627
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-21 15:15:43 +0500

    Принципы ООП

---
+++ day_11/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_11
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal eating");
+        }
+    }
+}
