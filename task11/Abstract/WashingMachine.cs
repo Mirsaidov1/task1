1fa1f71960b766ec3bd45b0205fab5ac8cd42656
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-22 00:10:34 +0500

    ДЗ_Абстрактные_классы

---
+++ task11/Abstract/WashingMachine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task11.Abstract
+{
+    public abstract class WashingMachine : Appliance
+    {
+        public override void TurnOn()
+        {
+            Console.WriteLine("Включение");
+        }
+    }
+}
