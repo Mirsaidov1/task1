081a48fa5a7972460f8eab958669ea80d26e2048
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-22 09:15:16 +0500

    Дополнительное задание

---
+++ task11/models/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task11.models
+{
+    public class Dog : Animal
+    {
+        public override void Sleep()
+        {
+            Console.WriteLine("Dog sleep");
+        }
+        public void Bark()
+        {
+            Console.WriteLine("Dog is barking");
+        }
+
+    }
+}
