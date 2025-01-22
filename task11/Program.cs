2c422019dfd33b001cb9fb09baad8f1664dabb71
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-22 09:28:26 +0500

    ДЗ_День_11

--- task11/Program.cs
+++ task11/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using task11.Abstract;
+using task11.models;
+// Интерфейсы
+Car car = new Car();
+car.Move(100);
+Robot robot = new Robot();
+robot.Move(200);
+// Абстрактные классы
+Appliance machine = new WashingMachine();
+machine.TurnOff();
+machine.TurnOn();
+Appliance refr = new Refrigerator();
+refr.TurnOn();
+refr.TurnOff();
+//Дополнительное задание
+Animal dog = new Dog();
+dog.Sleep();
+((Dog)dog).Bark();
+Animal cat = new Cat();
+cat.Sleep();
+((Cat)cat).Sound();
