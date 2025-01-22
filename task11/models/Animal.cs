a850d38faff1c4766f9417fe35c8511900f90d16
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-22 09:14:29 +0500

    Дополнительное задание

---
+++ task11/models/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace task11.models
+{
+    public class Animal
+    {
+        public virtual void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+
+        public virtual void Sleep()
+        {
+            Console.WriteLine("Animal sleep");
+        }
+    }
+}
