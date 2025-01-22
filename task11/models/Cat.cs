aeae22401fb5e4db228de2af1b95551c2b894aac
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-22 09:14:59 +0500

    Дополнительное задание

---
+++ task11/models/Cat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace task11.models
+{
+    public class Cat : Animal
+    {
+        public void Sound()
+        {
+            Console.WriteLine("my my my");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("Cat sleep");
+        }
+    }
+}
