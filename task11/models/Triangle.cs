33f0e16426bdad5481234819172b0ee726c008ae
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-23 14:46:47 +0500

    Дополнительные задания

---
+++ task11/models/Triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task11.models
+{
+    public class Triangle : Shape
+    {
+        private double s = 0;
+        private double p = 0;
+        public override void CalculateArea()
+        {
+            int a = 10;
+            int b = 10;
+            int c = 10;
+
+            int p = 12;
+
+            s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            Console.WriteLine($"Площадь треугольника: {s}");
+        }
+
+        public override void CalculatePerimetr()
+        {
+            int a = 2;
+            int b = 4;
+            int c = 6;
+
+            p = a + b + c;
+            Console.WriteLine($"периметр треугольника: {p}");
+        }
+    }
+}
