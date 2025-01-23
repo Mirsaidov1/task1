f74eb7080e700c5fe844f972544513d07e1ddcb8
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-23 14:46:26 +0500

    Дополнительные задания

--- task11/models/Rectangle.cs
+++ task11/models/Rectangle.cs
@@ -8,9 +8,22 @@ namespace task11.models
 {
     public class Rectangle : Shape
     {
-        public void CalculateArea()
+        private int s = 0;
+        private int p = 0;
+        public override void CalculateArea()
         {
+            int a = 6;    
+            int b = 4;
+            s = a * b;
+            Console.WriteLine($"Площадь прямоугольника: {s}");
+        }
 
+        public override void CalculatePerimetr()
+        {
+            int a = 5;
+            int b = 4;
+            p = (a + b) * 2;
+            Console.WriteLine($"Периметр прямоугольника: {p}");
         }
     }
 }
