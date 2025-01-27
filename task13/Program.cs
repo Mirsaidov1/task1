78095e282ddc53a3da52f2558cd26cbb1c842e88
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-27 13:36:34 +0500

    ДЗ_День_13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Обработка пользовательских данных
+try
+{
+    Console.WriteLine("Имя: ");
+    string name = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(name))
+    {
+        throw new ArgumentNullException("Пользователь ввел пустое поле.");
+    }
+
+    Console.WriteLine("Возраст: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+    if (age < 18 || age > 65)
+    {
+        throw new ArgumentOutOfRangeException("Возраст должен быть от 18 до 65.");
+    }
+    
+    Console.WriteLine("Зарплата: ");
+    double salary = Convert.ToDouble(Console.ReadLine());
+    if (salary <= 0)
+    {
+        throw new ArgumentException("Зарплата меньше нуля.");
+    }
+    Console.WriteLine($" Сотрудник: {name}\n Возраст: {age}\n Зарплата: {salary}");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine($"Ошибка строка пустая: {ex.Message}");
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine($"Ошибка возраста: {ex.Message}");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Ошибка зарплаты: {ex.Message}");
+}
