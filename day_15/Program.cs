fc756183e965661c19411b990191e28c4aa2acfc
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-30 15:32:51 +0500

    Асинхронные и Синхронные методы

---
+++ day_15/Program.cs
@@ -0,0 +1,9 @@
+﻿static async Task<string> GetHttpContentAsync(string url)
+{
+    using HttpClient client = new HttpClient();
+    return await client.GetStringAsync(url);
+}
+
+string content = await
+GetHttpContentAsync("https://jsonplaceholder.typicode.com/posts");
+Console.WriteLine(content);
