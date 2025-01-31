c05c4b239d5ecbeed54d6402cce63b4fe1917470
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-01-31 11:26:15 +0500

    a

---
+++ day_9/models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Product
+{
+	public string Name { get; set; }
+	public int Price { get; set; }
+	public int Quantity { get; set; }
+
+
+	public void Deconstruct(out string name, out int price, out int quantity)
+	{
+		name = Name;
+		price = Price;
+		quantity = Quantity;
+	}
+
+	
+}
