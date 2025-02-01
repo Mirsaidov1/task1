5bc9c979ba16cdebe4f01dbe84ce8f66a7ec8ec4
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-01 18:19:51 +0500

    ДЗ_День_14

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,1 +1,17 @@
 ﻿
+//var numbers = new List<int>();
+//int sum = 0;
+
+//Parallel.For(1, 101, i =>
+//{
+//    sum = i * i;
+//    numbers.Add(i);
+//} );
+
+//foreach (var number in numbers)
+//{
+//    Console.WriteLine(number);
+//}
+
+//Console.WriteLine($"Сумма квадратов: {sum}");
+
