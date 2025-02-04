4a7b30b394d22c027788fa445b59cc242ae65a1c
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-04 15:28:28 +0500

    День_15

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+List<int> array = new List<int> { 5, 2, 3, 5, 6 };
+List<int> result = new List<int>();
+foreach (var item in array)
+{
+
+    if (!result.Contains(item))
+    {
+        result.Add(item);
+    }
+}
+int element = array[0];
+foreach (var i in result)
+{
+    if (i <= element)
+    {
+        element = i;
+    }
+
+}
+Console.WriteLine(element);
+Console.WriteLine(string.Join(", ", result));
