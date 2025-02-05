be03cfb4d1a4125b178b1b422b7c31cf6cafa189
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-05 15:17:36 +0500

    Класс User

---
+++ Linq/models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class User
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public int Age { get; set; }
+
+	public bool IsValidate()
+	{
+		return Age > 0 && Age < 120;
+	}
+
+	public string Email { get; set; }
+	public string Password { get; set; }
+
+	public User()
+	{
+	}
+}
