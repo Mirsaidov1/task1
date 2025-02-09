c691e28b3434f15641ec1460e54b0409410b1484
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-09 23:56:06 +0500

    ДЗ_День_17

--- task16_Linq/Program.cs
+++ task16_Linq/Program.cs
@@ -2,7 +2,7 @@
 List<People> peoples = new List<People>
 {
     new People{Name = "Bobur" },
-    new People{Name = "Asadbek" },
+    new People{Name = "Ulugbek" },
     new People{Name = "Maks" },
     new People{Name = "Firuz" },
     new People{Name = "Rustamdzon" },
@@ -31,8 +31,8 @@ List<Order> orders = new List<Order>
 {
     new Order{OrderId = 1, CustomerName = "Rustam", Amount = 2500.10 },
     new Order{OrderId = 2, CustomerName = "Dilshod", Amount = 3999.9 },
-    new Order{OrderId = 3, CustomerName = "Aziz", Amount = 1899.99 },
-    new Order{OrderId = 4, CustomerName = "Asadbek", Amount = 1500.00 },
+    new Order{OrderId = 3, CustomerName = "Ulugbek", Amount = 1899.99 },
+    new Order{OrderId = 4, CustomerName = "ad", Amount = 1500.00 },
     new Order{OrderId = 5, CustomerName = "Anvar", Amount = 500.10 },
     new Order{OrderId = 6, CustomerName = "Umed", Amount = 200.80 },
     new Order{OrderId = 7, CustomerName = "Kamol", Amount = 3200.50 },
@@ -42,7 +42,7 @@ List<Order> orders = new List<Order>
 var highAmount = orders.Where(x => x.Amount > 500);
 Console.WriteLine("\nЗаказы больше 500: ");
 foreach (var order in highAmount)
-    Console.WriteLine($"{order.OrderId} {order.Amount}");
+    Console.WriteLine($"{order.OrderId}) {order.Amount}");
 
 var sumAmount = orders.Sum(x => x.Amount);
 Console.WriteLine($"\nСумма заказов: {sumAmount}");
